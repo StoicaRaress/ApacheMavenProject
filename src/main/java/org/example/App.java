378ef734c0b74c7a898d06426c82ae826668c3a8
StoikR <rares.s@sliide.com>
2023-05-15 16:43:36 +0300

    add stuff

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws MalformedURLException, InterruptedException {
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setDeviceName("emulator-5554")
+                .setApp(System.getProperty("/Users/rares/Documents/GitHub/ApacheMavenProject/src/main/resources/app-debug.apk"))
+                .eventTimings();
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        Thread.sleep(10000);
+        driver.findElement(By.id("random"));
         System.out.println( "Hello World!" );
     }
 }
